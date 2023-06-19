6b1e4407ae6732a21717d837f0dd050d9891717c
MateoMCuevas <135647011+MateoMCuevas@users.noreply.github.com>
2023-06-19 01:46:05 -0300

    Update EmpresaVuelo.java
    cambio en las funciones de buscar vuelos

--- EmpresaVuelo.java
+++ EmpresaVuelo.java
@@ -16,26 +16,27 @@ public class EmpresaVuelo {
 
     //Metodos
 
-    //Funcion que busca un vuelo segun su pais de origen
-    public HashSet<Vuelo> buscarVuelo (String paisOrigen){
-        HashSet<Vuelo> vuelosMismoOrigen = new HashSet<>();
-        Iterator it = vuelos.iterator();
-        while (it.hasNext()){
-            Vuelo vuelo = (Vuelo) it.next();
-            if (vuelo.getOrigen().equalsIgnoreCase(paisOrigen)) {
-                vuelosMismoOrigen.add(vuelo);
-            }
-        }
-        return vuelosMismoOrigen;
-    }
-
-    //Funcion que busca los vuelos que se encuentran disponibles
-    public HashSet<Vuelo> buscarVuelo (boolean disponibilidad){
+    //Funcion que busca un vuelo segun su pais de origen y destino
+    public HashSet<Vuelo> buscarVuelo (String paisOrigen,String paisDestino){
         HashSet<Vuelo> vuelosDisponibles = new HashSet<>();
         Iterator it = vuelos.iterator();
         while (it.hasNext()){
             Vuelo vuelo = (Vuelo) it.next();
-            if (vuelo.isDisponibilidad() == disponibilidad) {
+            if (vuelo.isDisponibilidad() && vuelo.getOrigen().equalsIgnoreCase(paisOrigen) && vuelo.getDestino().equalsIgnoreCase(paisDestino)) {
+                vuelosDisponibles.add(vuelo);
+            }
+        }
+        return vuelosDisponibles;
+    }
+    
+   
+    //Funcion que busca vuelos por rango de precio
+    public HashSet<Vuelo> buscarVuelo (float precioMin,float precioMax){
+        HashSet<Vuelo> vuelosDisponibles = new HashSet<>();
+        Iterator it = vuelos.iterator();
+        while (it.hasNext()){
+            Vuelo vuelo = (Vuelo) it.next();
+            if (vuelo.isDisponibilidad() && precioMin<= vuelo.getPrecio() && vuelo.getPrecio()<=precioMax) {
                 vuelosDisponibles.add(vuelo);
             }
         }

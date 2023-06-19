aa6d2916b5a7c8c5f215765ac456beb87d18d8e1
MateoMCuevas <135647011+MateoMCuevas@users.noreply.github.com>
2023-06-19 02:51:11 -0300

    Update Main.java
    añadimos los menus para buscar vuelos

--- Main.java
+++ Main.java
@@ -71,6 +71,88 @@ public class Main {
             System.out.println(e);
         }
     }
+       //funcion menu para la busqueda de vuelos y alojamientos
+    public static void menu(Usuario usuario)
+    {
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
+        do
+        {
+            //EmpresaVuelo empresaVuelo=new EmpresaVuelo();
+            System.out.println("Ingrese la opcion que desea realizar: ");
+            System.out.println("OPCION 1: Buscar vuelos");
+            System.out.println("OPCION 2: Buscar alojamiento");
+            System.out.println("OPCION 3: Salir");
+            int opcion = scanner.nextInt();
+            switch (opcion)
+            {
+                case 1:
+                    System.out.println("Ingrese el origen");
+                    String paisOrigen=scanner.nextLine();
+                    System.out.println("Ingrese el destino");
+                    String paisDestino=scanner.nextLine();
+                    System.out.println("Ingrese la fecha de salida");
+                    String fechaSalida=scanner.nextLine();
+
+                    //HashSet setVuelosDestino=empresaVuelo.buscarVuelo(paisOrigen,paisDestino);
+                    //System.out.println(setVuelos.toString());
+                    menu2();
+
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+            }
+
+        }while(salir==false);
+
+    }
+    //funcion para el filtrado de los vuelos
+    public static void menu2(Usuario usuario,EmpresaVuelo empresaVuelo,HashSet setVuelosDestino)
+    {
+        Scanner scanner = new Scanner(System.in);
+        boolean salir = false;
+        do
+        {
+            System.out.println("Desea filtrar por:");
+            System.out.println("OPCION 1: Por clase de vuelo");
+            System.out.println("OPCION 2: Por rango de precio");
+            System.out.println("OPCION 3: Por aerolinea");
+            System.out.println("OPCION 4: No filtrar");
+            int opcion = scanner.nextInt();
+            switch (opcion)
+            {
+                case 1:
+                    System.out.println("LAS CLASES DE LOS VUELOS SON:" +
+                            "PRIMERA CLASE" +
+                            "EJECUTIVA" +
+                            "ECONOMICA");
+                    String clase=scanner.nextLine();
+                    HashSet setVuelosFiltrados=empresaVuelo.buscarVuelo(clase,setVuelosDestino);
+                    System.out.println(setVuelosFiltrados.toString());
+                    System.out.println("desea reservar alguno");
+                    String rta=scanner.nextLine();
+                    if(rta.equalsIgnoreCase("si"))
+                    {
+                        System.out.println("ingrese el id del vuelo que quiera reservar");
+                        Vuelo vueloAReservar//hacer funcion para buscar por id
+                        Reserva reserva=empresaVuelo.reservarVuelo(Vuelo vueloAReservar);
+                        usuario.agregarReserva(reserva);
+                    }
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+            }
+
+        }while(salir==false);
+    }
     public static Usuario crearUsuario()
     {
         Scanner scanner = new Scanner(System.in);

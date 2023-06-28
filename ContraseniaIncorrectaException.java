98dddc32560e967ba4cb9898a59e35776a932953
moronnahuu1 <137028496+moronnahuu1@users.noreply.github.com>
2023-06-27 23:23:07 -0300

    Delete ContraseniaIncorrectaException.java

--- ContraseniaIncorrectaException.java
+++
@@ -1,7 +0,0 @@
-package LogIn.Excepciones;
-
-public class ContraseniaIncorrectaException extends Exception{
-    public ContraseniaIncorrectaException(String message) {
-        super(message);
-    }
-}

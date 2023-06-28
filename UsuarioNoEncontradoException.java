f6d26d148a70fc43cdb5c39b7052a033a59ebf03
moronnahuu1 <137028496+moronnahuu1@users.noreply.github.com>
2023-06-27 23:25:47 -0300

    Delete UsuarioNoEncontradoException.java

--- UsuarioNoEncontradoException.java
+++
@@ -1,7 +0,0 @@
-package LogIn.Excepciones;
-
-public class UsuarioNoEncontradoException extends Exception{
-    public UsuarioNoEncontradoException(String message) {
-        super(message);
-    }
-}
